1a130a19b5fd72aeb71785f4c80ab2648eba48d1
Richard Stelling <richard@empiricalmagic.com>
2013-11-11 10:23:40 +0000

    - Added stub static lib project

---
+++ Projects/ada/ada/ada.h
@@ -0,0 +1,13 @@
+//
+//  ada.h
+//  ada
+//
+//  Created by Richard Stelling on 11/11/2013.
+//  Copyright (c) 2013 The Ada Analytics Cooperative. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ada : NSObject
+
+@end
---
+++ Projects/ada/ada/ada.m
@@ -0,0 +1,13 @@
+//
+//  ada.m
+//  ada
+//
+//  Created by Richard Stelling on 11/11/2013.
+//  Copyright (c) 2013 The Ada Analytics Cooperative. All rights reserved.
+//
+
+#import "ada.h"
+
+@implementation ada
+
+@end
